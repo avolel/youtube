b6f4500f521cc5ef09e6e06f25ea72ec08c050ba
Andre Volel <avolel@gmail.com>
2023-08-12 22:20:46 -0400

    Script Downloads and converts in one action.

--- youtubedownload.py
+++ youtubedownload.py
@@ -3,6 +3,7 @@ from moviepy.editor import *
 import sys, getopt
 import argparse
 import os
+import traceback
 
 viddir_list = []
 
@@ -11,54 +12,34 @@ def Main(argv):
     try:
         argParser = argparse.ArgumentParser()
         argParser.add_argument("-u","--URL", type=str, help="Youtube Video URL")
-        argParser.add_argument("-l","--LOCATION", type=str, help="Video Location")
         argParser.parse_args()
-        opts, args = getopt.getopt(argv,"u:l:")
+        opts, args = getopt.getopt(argv,"u:")
+
         for opt, arg in opts:
-            if(opt == '-u'):            
-                Download(arg)
-            elif(opt == '-l'):
-                viddir_list = os.listdir(f'{arg}\\')
-                loopContinue = True
-                while loopContinue:
-                    VideoMenu()
-                    choice = input(f"Which video would you like to convert to an MP3 [0 - {len(viddir_list) - 1}]: ")
-                    if choice.isnumeric():
-                        if int(choice) >= 0 and int(choice) <= len(viddir_list) - 1:
-                            ConvertMP4toMP3(viddir_list[int(choice)], f'{arg}\\')
-                            loopContinue = False
-                        else:
-                            print("Invalid Option. Try Again.")
-                    else:
-                        print("Invalid Option. Try Again.")
-    except getopt.GetoptError:
-        print('youtubedownload.py [-h]')
+            if(opt == '-u'):
+                print(f'Downloading {arg}.')        
+                fileName = Download(arg)
+                print("Download Completed Successfully.")
+                print("Converting Video to MP3.")                                  
+                ConvertMP4toMP3(fileName)
+                print("Video Converted to MP3 successfully.")
+    except Exception as e:
+        print('An Exception occurred:', e)
+        traceback.print_exc()
         sys.exit(2)
 
-def Download(link):
-    try:
+def Download(link):        
         youTubeobj = YouTube(link, use_oauth=True, allow_oauth_cache=True)
+        file_name = youTubeobj.streams[0].default_filename
         youTubeobj = youTubeobj.streams.get_highest_resolution()
-        youTubeobj.download(f'{os.getcwd()}\\videos\\')
-        print(f"{link} Download Completed Successfully.")
-    except:
-        print("Opps! Something went wrong. Cannot download Youtube Video...")
+        youTubeobj.download(f'{os.getcwd()}\\videos\\')        
+        return file_name.replace(".3gpp",".mp4")
 
-def ConvertMP4toMP3(file, dir):
+def ConvertMP4toMP3(file):    
     mp3file = file.replace(".mp4",".mp3")
     if(os.path.exists(f'{os.getcwd()}\\mp3\\{mp3file}') == False):
-        video = VideoFileClip(f'{dir}\\{file}')
+        video = VideoFileClip(f'videos\\{file}')
         video.audio.write_audiofile(f'{os.getcwd()}\\mp3\\{mp3file}')
-
-def VideoMenu():
-    global viddir_list
-    index = 0    
-    print(30 * "-", "Choose a Video", 30 * "-")
-    for file in viddir_list:
-        print(f"{index}. {file}\n")
-        index+=1
-    print(67 * "-")
-
 
 if(__name__ == "__main__"):
     Main(sys.argv[1:])
