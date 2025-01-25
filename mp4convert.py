d8aef8b6fc0c47902f990e0ca7ff68316258e976
Andre Volel <avolel@gmail.com>
2025-01-25 10:20:44 -0500

    create a hot folder for vids

--- mp4convert.py
+++ mp4convert.py
@@ -1,9 +1,11 @@
 from moviepy.editor import *
 import os
 
-directory = os.fsencode(f'{os.getcwd()}\\videos')
+directory = os.fsencode(f'{os.getcwd()}\\vids')
 for file in os.listdir(directory):
     filename = os.fsdecode(file)
     mp3file = filename.replace(".mp4",".mp3")
-    video = VideoFileClip(f'videos\\{filename}')
+    video = VideoFileClip(f'vids\\{filename}')
     video.audio.write_audiofile(f'{os.getcwd()}\\mp3\\{mp3file}')
+    video.close()
+    os.remove(f'{os.getcwd()}\\vids\\{filename}')
